353c4709f3502862e8011dfeeef95b7da0b35a36
sfinks <urasfinks@yandex.ru>
2023-10-01 13:03:08 +0300

    Добавлена автоматическая конфигурация редиректа http -> https

--- src/main/java/ru/jamsys/Configuration.java
+++ src/main/java/ru/jamsys/Configuration.java
@@ -1,18 +1,26 @@
 package ru.jamsys;
 
+import lombok.Setter;
 import org.apache.catalina.Context;
 import org.apache.catalina.connector.Connector;
 import org.apache.tomcat.util.descriptor.web.SecurityCollection;
 import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.socket.config.annotation.*;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 import ru.jamsys.mistercraft.ControllerWebSocket;
 
 @org.springframework.context.annotation.Configuration
 @EnableWebSocket
 public class Configuration implements WebSocketConfigurer {
+
+    @Setter
+    @Value("${server.ssl.enabled:false}")
+    private Boolean serverSslEnabled;
 
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(new ControllerWebSocket(), "/socket/*");
@@ -28,15 +36,19 @@ public class Configuration implements WebSocketConfigurer {
         TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
             @Override
             protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint(USER_CONSTRAINT);
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
+                if (serverSslEnabled) {
+                    SecurityConstraint securityConstraint = new SecurityConstraint();
+                    securityConstraint.setUserConstraint(USER_CONSTRAINT);
+                    SecurityCollection collection = new SecurityCollection();
+                    collection.addPattern("/*");
+                    securityConstraint.addCollection(collection);
+                    context.addConstraint(securityConstraint);
+                }
             }
         };
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        if (serverSslEnabled) {
+            tomcat.addAdditionalTomcatConnectors(redirectConnector());
+        }
         return tomcat;
     }
 
