8b5d421c8245d968449531af0cceee51183d39f5
urasfinks <urasfinks@yandex.ru>
2024-08-29 13:45:56 +0300

    + Sync

---
+++ src/main/java/ru/jamsys/web/http/Sync.java
@@ -0,0 +1,38 @@
+package ru.jamsys.web.http;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.jamsys.PromiseExtension;
+import ru.jamsys.core.component.ServicePromise;
+import ru.jamsys.core.promise.Promise;
+import ru.jamsys.core.promise.PromiseGenerator;
+import ru.jamsys.core.web.http.HttpHandler;
+
+/*
+ * Генерация кода для авторизации. Если нет пользователя -> insert. Отправляем код на указанную почту.
+ * */
+@Component
+@RequestMapping
+public class Sync implements PromiseGenerator, HttpHandler {
+
+    @Getter
+    @Setter
+    private String index;
+
+    private final ServicePromise servicePromise;
+
+    public Sync(ServicePromise servicePromise) {
+        this.servicePromise = servicePromise;
+    }
+
+    @Override
+    public Promise generate() {
+        return servicePromise.get(index, 1000L)
+                .extension(PromiseExtension::thenSelectUuidDevice)
+
+                .extension(PromiseExtension::addTerminal);
+    }
+
+}
