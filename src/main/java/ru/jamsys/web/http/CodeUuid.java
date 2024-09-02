4c2da3af2e3e84e424ed702be58292a684fdcd6e
sfinks <urasfinks@yandex.ru>
2024-09-02 21:01:09 +0300

    CodeUuid проверен

--- src/main/java/ru/jamsys/web/http/CodeUuid.java
+++ src/main/java/ru/jamsys/web/http/CodeUuid.java
@@ -10,7 +10,6 @@ import ru.jamsys.PromiseExtension;
 import ru.jamsys.ResponseObject;
 import ru.jamsys.core.App;
 import ru.jamsys.core.component.ServicePromise;
-import ru.jamsys.core.extension.builder.HashMapBuilder;
 import ru.jamsys.core.extension.http.ServletHandler;
 import ru.jamsys.core.flat.util.JsonSchema;
 import ru.jamsys.core.flat.util.UtilFileResource;
@@ -48,16 +47,13 @@ public class CodeUuid implements PromiseGenerator, HttpHandler {
                 .extension(PromiseExtension::addResponseObject)
                 .extension(PromiseExtension::thenSelectIdUser)
                 .then("init", (_, promise) -> {
-                    //{"uuid":"uudData"}
+                    //{"code": 267936}
                     ServletHandler servletHandler = promise.getRepositoryMapClass(ServletHandler.class);
                     String data = servletHandler.getRequestReader().getData();
-                    JsonSchema.validate(data, UtilFileResource.getAsString("schema/http/UuidByCode.json"), "UuidByCode.json");
+                    JsonSchema.validate(data, UtilFileResource.getAsString("schema/http/CodeUuid.json"), "CodeUuid.json");
                     Map<String, Object> parsedJson = UtilJson.getMapOrThrow(data);
 
                     Integer code = (Integer) parsedJson.get("code");
-                    if (code == null) {
-                        throw new RuntimeException("Code is empty");
-                    }
                     ManagerCodeLinkItem find = App.get(ManagerCodeLink.class).getByCode(code);
                     if (find == null) {
                         throw new Exception("Code not found in " + ManagerCodeLink.class.getName());
@@ -65,26 +61,6 @@ public class CodeUuid implements PromiseGenerator, HttpHandler {
                     promise.getRepositoryMapClass(ResponseObject.class)
                             .append("code", find.getCode())
                             .append("uuid", find.getUuidData());
-                })
-                .thenWithResource("db", JdbcResource.class, "default", (_, promise, jdbcResource) -> {
-                    String uuidData = promise.getRepositoryMap("uuidData", String.class);
-                    List<Map<String, Object>> execute = jdbcResource.execute(
-                            new JdbcRequest(Data.CHECK_PERMISSION_SOCKET_DATA)
-                                    .addArg("id_user", promise.getRepositoryMap("id_user", Integer.class))
-                                    .addArg("uuid_device", promise.getRepositoryMap("uuid_device", String.class))
-                                    .addArg("uuid_data", uuidData)
-                    );
-                    if (execute.isEmpty()) {
-                        throw new RuntimeException("Permission denied");
-                    }
-
-                })
-                .then("addLink", (_, promise) -> {
-                    String uuidData = promise.getRepositoryMap("uuidData", String.class);
-                    ManagerCodeLinkItem add = App.get(ManagerCodeLink.class).add(uuidData);
-                    promise.getRepositoryMapClass(ResponseObject.class)
-                            .append("code", add.getCode())
-                            .append("uuid", add.getUuidData());
                 })
                 .extension(PromiseExtension::addTerminal);
     }
