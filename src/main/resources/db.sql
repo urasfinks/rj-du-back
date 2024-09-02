9ee3a61577237f89a7c3177d88a21b400b4537fd
sfinks <urasfinks@yandex.ru>
2024-09-02 20:38:52 +0300

    + revision seq

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,3 +1,10 @@
+CREATE SEQUENCE IF NOT EXISTS public.revision
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 9223372036854775807
+    CACHE 1;
+
 -- Table: public.user
 
 -- DROP TABLE IF EXISTS public."user";
