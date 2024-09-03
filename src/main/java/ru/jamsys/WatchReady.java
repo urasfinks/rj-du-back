c8784725d4638bed2c460a008af1f8deffdd4261
sfinks <urasfinks@yandex.ru>
2024-09-03 15:50:04 +0300

    Рефактор *.properties

---
+++ src/main/java/ru/jamsys/WatchReady.java
@@ -0,0 +1,19 @@
+package ru.jamsys;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import ru.jamsys.core.App;
+
+@SpringBootApplication
+public class WatchReady {
+
+    public static void main(String[] args) {
+        App.springSource = WatchReady.class;
+        App.getRunBuilder()
+                .add("spring.config.location", "classpath:application.properties,classpath:/properties/watch-ready/application.properties")
+                .runCore();
+
+        System.out.println("APP NAME: " + App.applicationName);
+
+    }
+
+}
